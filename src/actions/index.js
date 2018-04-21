357fb27a24ca00bee7fe16cb7a1b9a81a31ec5f5
buncis <buncismamen@gmail.com>
2018-04-22 01:23:34 +0700

    hore udah bisa ternyata cara gua ngebuat action thunk itu salah coi https://stackoverflow.com/questions/45829850/react-redux-with-thunk-getstate-is-not-a-function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,26 +1,27 @@
 import axios from 'axios';
 import { getIsFetching } from '../reducers';
 
-export const fetchPosts = (dispatch, getState) => {
-
-  return dispatch => {
-    dispatch({
-      type: 'FETCH_POSTS_REQUEST',
-    });
-  
-    return axios.get('http://localhost:3000/posts.json').then(
-      response => {
-        dispatch({
-          type: 'FETCH_POSTS_SUCCESS',
-          response: response.data,
-        });
-      },
-      error => {
-        dispatch({
-          type: 'FETCH_POSTS_FAILURE',
-          message: error.message || 'Something went wrong.'
-        });
-      }
-    );
+export const fetchPosts = () => (dispatch, getState) => {
+  if (getIsFetching(getState())) {
+    return Promise.resolve();
   }
+
+  dispatch({
+    type: 'FETCH_POSTS_REQUEST',
+  });
+
+  return axios.get('http://localhost:3000/posts.json').then(
+    response => {
+      dispatch({
+        type: 'FETCH_POSTS_SUCCESS',
+        response: response.data,
+      });
+    },
+    error => {
+      dispatch({
+        type: 'FETCH_POSTS_FAILURE',
+        message: error.message || 'Something went wrong.'
+      });
+    }
+  );
 };
